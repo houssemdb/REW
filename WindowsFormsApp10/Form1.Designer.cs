d0a2609561a29f707aa25cac9113b862c2479c06
houssem <houssem.dbeibia@coficab.com>
2018-12-12 16:58:14 -0700

    Add project files.

---
+++ WindowsFormsApp10/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label18 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Leave += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(62, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Leave += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(62, 143);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Leave += new System.EventHandler(this.label6_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(62, 244);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Leave += new System.EventHandler(this.label7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IN 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "IN 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "IN 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "OUT 1";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(222, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Item 1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(86, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(222, 102);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "item2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(222, 244);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "item 4 ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(222, 150);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "item3";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(355, 54);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(28, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Lot1";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(355, 105);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(28, 13);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "Lot2";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(355, 150);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(28, 13);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "Lot3";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(355, 244);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(28, 13);
+            this.label13.TabIndex = 16;
+            this.label13.Text = "Lot4";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(453, 54);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(55, 13);
+            this.label14.TabIndex = 17;
+            this.label14.Text = "Qunatity 1";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(453, 109);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(55, 13);
+            this.label15.TabIndex = 18;
+            this.label15.Text = "Qunatity 2";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(453, 150);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(55, 13);
+            this.label16.TabIndex = 19;
+            this.label16.Text = "Qunatity 3";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(453, 244);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(55, 13);
+            this.label17.TabIndex = 20;
+            this.label17.Text = "Qunatity 4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(690, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 39);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Print ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(724, 47);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 22;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(668, 50);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(50, 13);
+            this.label18.TabIndex = 23;
+            this.label18.Text = "Printer IP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 395);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label18;
+    }
+}
+
---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string ok = @"^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR6,6~SD15^JUS^LRN^CI0^XZ
+^XA
+^MMT
+^PW1200
+^LL0600
+^LS0
+^FT212,342^A0N,228,494^FH\^FDOK^FS
+^PQ1,0,1,Y^XZ";
+        public string nok = @"^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR6,6~SD15^JUS^LRN^CI0^XZ
+^XA
+^MMT
+^PW1200
+^LL0600
+^LS0
+^FT108,360^A0N,228,494^FH\^FDNOK^FS
+^PQ1,0,1,Y^XZ
+";
+        public void SendData(string zpl)
+        {
+            NetworkStream ns = null;
+            Socket socket = null;
+            IPEndPoint printerIP = null;
+            String IP = textBox5.Text;
+            try
+            {
+                if (printerIP == null)
+                {
+                    /* IP is a string property for the printer's IP address. */
+                    /* 6101 is the common port of all our Zebra printers. */
+                    printerIP = new IPEndPoint(IPAddress.Parse(IP), 9100);
+                }
+
+                socket = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream,
+                    ProtocolType.Tcp);
+                socket.Connect(printerIP);
+
+                ns = new NetworkStream(socket);
+
+                byte[] toSend = Encoding.ASCII.GetBytes(zpl);
+                ns.Write(toSend, 0, toSend.Length);
+            }
+            finally
+            {
+                if (ns != null)
+                    ns.Close();
+
+                if (socket != null && socket.Connected)
+                    socket.Close();
+            }
+        }
+        public void intia()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            label9.Text = "item3";
+            label7.Text = "item2";
+            label5.Text = "item1";
+            label8.Text = "item4";
+            label10.Text = "Lot1";
+            label11.Text = "Lot2";
+            label12.Text = "Lot3";
+            label13.Text = "Lot4";
+            label14.Text = "Qunatity 1";
+            label15.Text = "Qunatity 2";
+            label16.Text = "Qunatity 3";
+            label17.Text = "Qunatity 4";
+
+
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source = 192.168.100.155\\ERPLN; Initial Catalog = erplnfp9db; User ID = sa; Password = P@ssw0rd123");
+            con.Open();
+            string sql = @"select t_item,t_clot,t_qhds from twhwmd530502 where t_huid='" + textBox1.Text + "';";
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                label5.Text = reader.GetString(0);
+                label10.Text = reader.GetString(1);
+                label14.Text = reader.GetDouble(2).ToString();
+
+
+            }
+
+
+            con.Close();
+            
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source = 192.168.100.155\\ERPLN; Initial Catalog = erplnfp9db; User ID = sa; Password = P@ssw0rd123");
+            con.Open();
+            string sql = @"select t_item,t_clot,t_qhds from twhwmd530502 where t_huid='" + textBox2.Text + "';";
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                label7.Text = reader.GetString(0);
+                label11.Text = reader.GetString(1);
+                label15.Text = reader.GetDouble(2).ToString();
+
+
+            }
+
+
+            con.Close();
+
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source = 192.168.100.155\\ERPLN; Initial Catalog = erplnfp9db; User ID = sa; Password = P@ssw0rd123");
+            con.Open();
+            string sql = @"select t_item,t_clot,t_qhds from twhwmd530502 where t_huid='" + textBox3.Text + "';";
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                label9.Text = reader.GetString(0);
+                label12.Text = reader.GetString(1);
+                label16.Text = reader.GetDouble(2).ToString();
+
+
+            }
+
+
+            con.Close();
+
+        }
+        private void label7_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source = 192.168.100.155\\ERPLN; Initial Catalog = erplnfp9db; User ID = sa; Password = P@ssw0rd123");
+            con.Open();
+            string sql = @"select t_item,t_clot,t_qhds from twhwmd530502 where t_huid='" + textBox4.Text + "';";
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                label8.Text = reader.GetString(0);
+                label13.Text = reader.GetString(1);
+                label17.Text = reader.GetDouble(2).ToString();
+            }
+
+
+            con.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((label8.Text== label9.Text) & (label8.Text == label7.Text) &(label8.Text == label5.Text))
+                { MessageBox.Show("OK");
+                this.SendData(ok);
+                }
+            else if ((label9.Text=="item3") & (label8.Text == label7.Text) & (label8.Text == label5.Text))
+            { MessageBox.Show("OK");
+                this.SendData(ok);
+            }
+            else if ((label9.Text == "item3") & ( label7.Text=="item2") & (label8.Text == label5.Text))
+            { MessageBox.Show("OK");
+                this.SendData(ok);
+            }
+            else
+            { MessageBox.Show("NOK");
+                this.SendData(nok);
+            }
+            
+            SqlConnection con = new SqlConnection("Data Source = 192.168.100.163; Initial Catalog = Rewinding_Data; User ID = sa; Password = 1234");
+            con.Open();
+            MessageBox.Show("conection ok");
+            
+            string sql = @"INSERT INTO dbo.Rew_output (Huidout,itemout,qtyout,huidin1,qtyin1,huidin2,qtyin2,huidin3,qtyin3,dtime) VALUES (" + textBox4.Text + "," + label8.Text + "," + label17.Text + "," + textBox1.Text  + "," + label14.Text + "," + textBox2.Text + "," + label15.Text + "," + textBox3.Text + "," + label16.Text + ",GETDATE());";
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+          
+
+            con.Close();
+            this.intia();
+        }
+    }
+}
